2cbfb89f290fd36b8ab8dd4f578d07f321bc582d
Jii Eu <jeyeyeu@pm.me>
2019-09-25 17:48:50 -0700

    fixed) indentation

--- algorithm/sorting/bubbleSort.rb
+++ algorithm/sorting/bubbleSort.rb
@@ -1,16 +1,16 @@
 def bubble_sort(arr)
     n = arr.size
-	for i in (0 ... n)
-		isSwapped = false
+    for i in (0 ... n)
+        isSwapped = false
 
-		for j in (0 ... n - i - 1)
-			if arr[j] > arr[j+1]
-				isSwapped = true
+        for j in (0 ... n - i - 1)
+            if arr[j] > arr[j+1]
+                isSwapped = true
                 arr[j], arr[j+1] = arr[j+1], arr[j]
             end
         end
-		if isSwapped == false
-			break
+        if isSwapped == false
+            break
         end
     end
 
