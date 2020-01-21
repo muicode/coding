0dd6cb05633c0afb2d6da53b5bf2b6d5f2ddf092
Jii Eu <jeyeyeu@pm.me>
2020-01-21 15:06:26 -0800

    palindrome modified

--- algorithm/palindrome/palindrome/palindrome.cpp
+++ algorithm/palindrome/palindrome/palindrome.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+#define LOWERCASE true
+#define UPPERCASE false
+
+void setCases(string &str, bool lowercase)
+{
+    const int SIZE = str.end() - str.begin();
+
+    for (int i=0; i<SIZE; ++i)
+    {
+        str[i] = (lowercase) ? tolower(str[i]) : toupper(str[i]);
+    }
+}
+
+void ignoreSpaces(string &str)
+{
+    const int SIZE = str.end() - str.begin();
+    string ret = "";
+
+    for (int i=0; i<SIZE; ++i)
+    {
+        if (str[i] !=  ' ')
+        {
+            ret += str[i];
+        }
+    }
+
+    str = ret;
+}
+
 bool isPalindrome(string str)
 {
-    const int SIZE = str.size();
+    setCases(str, LOWERCASE);
+    ignoreSpaces(str);
+
+    const int SIZE = str.end() - str.begin();
     const int HALF = SIZE >> 1;
 
     for (int i=0; i<HALF; ++i)
     {
-        if (str[i] != str[SIZE - i - 1])
+        if (str[i] != str[SIZE - 1 - i])
         {
             return false;
-        }   
+        }
     }
 
-    return true;
-}
-
-int main()
-{
-    string str;
-    cin >> str;
-
-    if (isPalindrome(str))
-    {
-        cout << "Yes" << endl;
-    }
-    else 
-    {
-        cout << "No" << endl;
-    }
-
-    return 0;
-}
