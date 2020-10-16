977423349d332d475dbf1eb73b2656181eb721e6
Jii Eu <jeyeyeu@pm.me>
2020-10-16 18:41:37 +0900

    Separate stack interface with and main

--- stack/array/stack.cpp
+++ stack/array/stack.cpp
@@ -1,91 +1,29 @@
 #include <iostream> 
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-#define SIZE 10
-
-template <class T>
-class Stack 
-{
-  private:
-    T *stack;
-    int top;
-    int capacity;
-
-  public:
-    Stack(int n) 
-    {
-      stack = new T[n+1];
-      top = 0;
-      capacity = n;
-    }
-
-    ~Stack() 
-    {
-      capacity = top = 0;
-      delete stack;
-    }
-
-    bool empty()
-    {
-      return top == 0;
-    }
-
-    void push(T val)
-    {
-      if (top == capacity)
-      {
-        cout << "Overflow...";
-        return;
-      }
-
-      top += 1;
-      stack[top] = val;
-    }
-
-    T pop() 
-    {
-      cout << "hello" << endl;
-      if(empty())
-      {
-        cout << "Underflow...";
-        return -1;
-      }
-      else
-      {
-        top -= 1;
-        return stack[top + 1];
-      }
-    }
-
-    void print() 
-    {
-      for(int i=1; i<=top; ++i)
-      {
-        cout << stack[i] << ' ';
-      }
-      cout << endl;
-    }
-};
+#include "stack.hpp"
 
 int main()
 {
   unsigned seed = time(0);
   srand(seed);
 
-  Stack<double> stack(SIZE);
+  Stack<int> stack(SIZE);
 
   for(int i=0; i<SIZE; ++i) 
   {
-    stack.push(rand() % 100 / 17.0);
+    stack.push(rand() % 100;
   }
 
   stack.print();
 
   while(!stack.empty())
   {
-    cout << stack.pop() << endl;
+    cout << stack.peek() << endl;
+    stack.pop();
   }
 
   stack.pop();
---
+++ stack/array/stack.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <iostream> 
+
+using std::cout;
+using std::endl;
+
+#define SIZE 10
+
+template <class T>
+class Stack 
+{
+  private:
+    T *stack;
+    int top;
+    int capacity;
+
+  public:
+    Stack(int n);
+    ~Stack();
+
+    void pop();
+    void push(T val);
+    T peek();
+    bool empty();
+    void print(); 
+};
+
+template <class T>
+Stack<T>::Stack(int n) 
+{
+  stack = new T[n+1];
+  top = 0;
+  capacity = n;
+}
+
+template <class T>
+Stack<T>::~Stack() 
+{
+  capacity = top = 0;
+  delete stack;
+}
+
+template <class T>
+bool Stack<T>::empty()
+{
+  return top == 0;
+}
+
+template <class T>
+void Stack<T>::push(T val)
+{
+  if (top == capacity)
+  {
+    cout << "Overflow...";
+    return;
+  }
+
+  top += 1;
+  stack[top] = val;
+}
+
+template <class T>
+void Stack<T>::pop() 
+{
+  if(empty())
+  {
+    cout << "Underflow...";
+    return;
+  }
+  top -= 1;
+}
+
+template <class T> 
+T Stack<T>::peek() 
+{
+  return stack[top];
+}
+
+template <class T> 
+void Stack<T>::print() 
+{
+  for(int i=1; i<=top; ++i)
+  {
+    cout << stack[i] << ' ';
+  }
+  cout << endl;
+}
